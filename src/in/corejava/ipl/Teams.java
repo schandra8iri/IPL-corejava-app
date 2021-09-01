d3d89251787c6f44eaf6633eb536c93b8f7d8ab0
schandra8iri <sandeep.chandra8iri@gmail.com>
2021-09-01 14:18:42 +0530

    Create a separate class for IPL team to print

---
+++ src/in/corejava/ipl/Teams.java
@@ -0,0 +1,17 @@
+package in.corejava.ipl;
+
+public class Teams {
+	
+	public void printTeams() {
+		
+		System.out.println(">> IPL TEAM - ROYAL CHALLENGERS BANGALORE");
+				System.out.println(" >> CAPTAIN - Virat Koli");
+						System.out.println(">> WICKET KEEPER - AB de villers");
+								System.out.println(">> BATSMAN - DevDUtt Padikal");
+										System.out.println(">> BOWLER - Yuzvendra Chahal");
+												System.out.println(">> Total of 266 sixes");
+														System.out.println(">> Net run rate is 0.171");
+																System.out.println(">> not been knock out yet");
+	}
+
+}
