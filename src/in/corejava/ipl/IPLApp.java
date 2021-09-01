4a5519e122e82364dd7aa867adb4c075209e7db8
schandra8iri <sandeep.chandra8iri@gmail.com>
2021-09-01 12:05:50 +0530

    Create a new class to print teams

---
+++ src/in/corejava/ipl/IPLApp.java
@@ -0,0 +1,25 @@
+package in.corejava.ipl;
+
+public class IPLApp {
+	
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("************* IPL 2021 *************");
+		
+		System.out.println(">> IPL TEAM - ROYAL CHALLENGERS BANGALORE");
+				System.out.println(" >> CAPTAIN - Virat Koli");
+						System.out.println(">> WICKET KEEPER - AB de villers");
+								System.out.println(">> BATSMAN - DevDUtt Padikal");
+										System.out.println(">> BOWLER - Yuzvendra Chahal");
+												System.out.println(">> Total of 266 sixes");
+														System.out.println(">> Net run rate is 0.171");
+																System.out.println(">> not been knock out yet");
+		
+
+	
+	}
+
+}
