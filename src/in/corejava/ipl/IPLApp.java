737075c17275b8c472483bc1fface53a1f7f171d
schandra8iri <sandeep.chandra8iri@gmail.com>
2021-09-01 15:12:23 +0530

    Create more methord to add behaviour to teams

--- src/in/corejava/ipl/IPLApp.java
+++ src/in/corejava/ipl/IPLApp.java
@@ -10,7 +10,34 @@ public class IPLApp {
 		System.out.println("************* IPL 2021 *************");
 
 		Teams teams = new Teams();
-		teams.printTeams();
+		//teams.printTeams();
+		
+		System.out.println(teams.getTeamName());
+		
+		System.out.println("PLAYERS:");
+		
+		String[] teamPlayers = teams.getPlayers();
+		
+		System.out.println(teamPlayers[0]);
+		System.out.println(teamPlayers[1]);
+		System.out.println(teamPlayers[2]);
+		System.out.println(teamPlayers[3]);
+		
+		System.out.println("RCS's Total number of Six is :" + teams.getTotalSix());
+		
+		System.out.println("RCS's Net run rate is :" + teams.getNetRunRate());
+		
+		System.out.println(" has RCB Knocked out :" + teams.isKnockOut());
+		
+		System.out.println("change sixez and NRR");
+		
+		teams.setNetRunRate(0.871);
+		teams.setTotalSix(299);
+		
+		System.out.println("RCS's Total number of Six is :" + teams.getTotalSix());
+		
+		System.out.println("RCS's Net run rate is :" + teams.getNetRunRate());
+
 
 	
 	}
--- src/in/corejava/ipl/Teams.java
+++ src/in/corejava/ipl/Teams.java
@@ -7,6 +7,48 @@ public class Teams {
 	public double netRunRate = 0.171;
 	public boolean isKnockOut = false;
 	
+	public String getTeamName() {
+		return teamName;
+	}
+
+
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	public int getTotalSix() {
+		return totalSix;
+	}
+
+	public void setTotalSix(int totalSix) {
+		this.totalSix = totalSix;
+	}
+
+	public double getNetRunRate() {
+		return netRunRate;
+	}
+
+	public void setNetRunRate(double netRunRate) {
+		this.netRunRate = netRunRate;
+	}
+
+	public boolean isKnockOut() {
+		return isKnockOut;
+	}
+
+	public void setKnockOut(boolean isKnockOut) {
+		this.isKnockOut = isKnockOut;
+	}
+
+	public String[] getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(String[] players) {
+		this.players = players;
+	}
+
 	public String [] players = {" >> CAPTAIN - Virat Koli", 
 			">> WICKET KEEPER - AB de villers", 
 			">> BATSMAN - DevDUtt Padikal",
@@ -14,7 +56,7 @@ public class Teams {
 	
 	public void printTeams() {
 		
-		System.out.println(teamName);
+		/*System.out.println(teamName);
 		
 		System.out.println("PLAYERS:");
 		
@@ -27,7 +69,7 @@ public class Teams {
 		
 		System.out.println("RCS's Net run rate is :" + netRunRate);
 		
-		System.out.println(" has RCB Knocked out :" + isKnockOut);
+		System.out.println(" has RCB Knocked out :" + isKnockOut);*/
 
 	}
 
