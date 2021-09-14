494c56eb972c0dd70851aff9ece22555f413ab4b
schandra8iri <sandeep.chandra8iri@gmail.com>
2021-09-14 14:12:00 +0530

    Create a class for staff

---
+++ src/in/corejava/ipl/Staff.java
@@ -0,0 +1,55 @@
+package in.corejava.ipl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Staff {
+	
+
+	
+public List<Staffer> staffList = new ArrayList<Staffer>();
+	
+	
+
+
+	public List<Staffer> getStaffList() {
+	return staffList;
+}
+
+
+
+public void setStaffList(List<Staffer> staffList) {
+	this.staffList = staffList;
+}
+
+
+
+	public Staff(List<Staffer> staffList) {
+	// TODO Auto-generated constructor stub#
+		this.staffList = staffList;
+}
+
+
+
+	@Override
+	public String toString() {
+		
+		String ListOfStaff = "" ;
+		
+		  for(Staffer staffer : staffList) {
+			  //System.out.println("from Players");
+			  
+			  //System.out.println(player.toString());
+			  ListOfStaff =  ListOfStaff.concat(staffer.toString());
+			  ListOfStaff =  ListOfStaff.concat(System.lineSeparator());
+			
+		}
+		 // System.out.println(ListOfPlayers);
+		  return ListOfStaff;
+	}
+
+	
+	
+	}
+
+
