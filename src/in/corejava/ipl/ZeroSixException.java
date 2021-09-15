76b78defb7f20177ccb9173da399b0ccd61ce5aa
schandra8iri <sandeep.chandra8iri@gmail.com>
2021-09-15 17:16:08 +0530

    modify players to create an exception

---
+++ src/in/corejava/ipl/ZeroSixException.java
@@ -0,0 +1,9 @@
+package in.corejava.ipl;
+
+public class ZeroSixException extends RuntimeException {
+
+	public ZeroSixException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
