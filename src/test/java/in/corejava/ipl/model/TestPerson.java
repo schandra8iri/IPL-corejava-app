8bd226686408cbce8bd44ec403f13f8d4f112a5f
schandra8iri <sandeep.chandra8iri@gmail.com>
2021-10-08 12:49:59 +0530

    testcase

---
+++ src/test/java/in/corejava/ipl/model/TestPerson.java
@@ -0,0 +1,79 @@
+package in.corejava.ipl.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPerson {
+
+	@Test
+	void testPersonStringStringIntStringGender() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPerson() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFirstName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFirstName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSecondName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetSecondName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetAge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPlace() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPlace() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetGender() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetGender() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFullName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
